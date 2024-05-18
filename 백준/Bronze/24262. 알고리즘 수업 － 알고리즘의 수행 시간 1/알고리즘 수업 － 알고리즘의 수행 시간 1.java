71ebaf91a325c4d6aad6c64b7bda4257989927aa
이원혁 <104248767+Won-Hyuk-Lee@users.noreply.github.com>
2024-05-18 20:18:09 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 120 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //어떤 정수를 넣더라도 한번에 처리가 되기때문에 상관이 없음...
+        System.out.println(1); //어떤 정수를 넣어도 처리횟수는 1회
+        System.out.println(0); //어떤 정수를 넣더라도 빅오표기법은 O(1)이기 때문에 최고차항은 0
+    }
+}
