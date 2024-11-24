7e946724e5b4cdfe41e3abe7fda7ceda07c6890e
이원혁 <104248767+Won-Hyuk-Lee@users.noreply.github.com>
2024-11-24 20:13:51 +0900

    [Bronze I] Title: 경우의 수의 합, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/30189. 경우의 수의 합/경우의 수의 합.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), m = read(), k = n + m + 1, cnt = 0;
+        while (k-- > 0) for (int i = 0, j; i <= n; i++) if ((j = k - i) >= 0 && j <= m) cnt++;
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
