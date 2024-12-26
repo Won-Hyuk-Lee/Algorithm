afdb55930ecd5e08f83168c01a1073d1b8551573
이원혁 <104248767+Won-Hyuk-Lee@users.noreply.github.com>
2024-12-26 22:13:10 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 124 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			System.out.println("You get " + (A / B) + " piece(s) and your dad gets " + (A % B) + " piece(s).");
+		}
+	}
+
+}
