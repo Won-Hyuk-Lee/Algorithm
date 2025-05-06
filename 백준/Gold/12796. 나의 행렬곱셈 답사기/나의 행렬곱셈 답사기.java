1bb68c01762b689a6894a33be23112b45b108efd
이원혁 <104248767+Won-Hyuk-Lee@users.noreply.github.com>
2025-05-06 16:46:42 +0900

    [Gold V] Title: 나의 행렬곱셈 답사기, Time: 188 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Gold/12796. 나의 행렬곱셈 답사기/나의 행렬곱셈 답사기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int K = scanner.nextInt();
+
+        System.out.println(3);
+        System.out.println("1 1 1 " + (K + 1));
+    }
+}
