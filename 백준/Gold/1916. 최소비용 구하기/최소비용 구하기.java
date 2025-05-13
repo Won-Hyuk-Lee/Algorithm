2879f1dc8e5ad81db4438853dc9bc5724c16609b
이원혁 <104248767+Won-Hyuk-Lee@users.noreply.github.com>
2025-05-13 09:51:36 +0900

    [Gold V] Title: 최소비용 구하기, Time: 472 ms, Memory: 50836 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Node implements Comparable<Node> {
+        int to, cost;
+        Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        List<Node>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph[u].add(new Node(v, c));
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if (cur.cost > dist[cur.to]) continue;
+            for (Node next : graph[cur.to]) {
+                if (dist[next.to] > cur.cost + next.cost) {
+                    dist[next.to] = cur.cost + next.cost;
+                    pq.add(new Node(next.to, dist[next.to]));
+                }
+            }
+        }
+
+        System.out.println(dist[end]);
+    }
+}
