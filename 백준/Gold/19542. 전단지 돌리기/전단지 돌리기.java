31304abeb89451b66f452ab21e9f2849d4e05b95
이원혁 <104248767+Won-Hyuk-Lee@users.noreply.github.com>
2025-06-03 16:58:24 +0900

    [Gold III] Title: 전단지 돌리기, Time: 444 ms, Memory: 66116 KB -BaekjoonHub

---
+++ 백준/Gold/19542. 전단지 돌리기/전단지 돌리기.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static int n, s, d, cnt;
+   static List<Integer>[] list;
+   static int[] depth;
+   
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       
+       n = Integer.parseInt(st.nextToken());
+       s = Integer.parseInt(st.nextToken());
+       d = Integer.parseInt(st.nextToken());
+       
+       depth = new int[n + 1];
+       list = new ArrayList[n + 1];
+       
+       for (int i = 1; i <= n; i++) {
+           list[i] = new ArrayList<>();
+       }
+       
+       for (int i = 0; i < n - 1; i++) {
+           st = new StringTokenizer(br.readLine());
+           int a = Integer.parseInt(st.nextToken());
+           int b = Integer.parseInt(st.nextToken());
+           list[a].add(b);
+           list[b].add(a);
+       }
+       
+       dfs(s, -1);
+       System.out.println(cnt << 1);
+   }
+   
+   static int dfs(int idx, int pa) {
+       for (int nxt : list[idx]) {
+           if (nxt != pa) {
+               depth[idx] = Math.max(depth[idx], dfs(nxt, idx) + 1);
+           }
+       }
+       
+       if (idx != s && depth[idx] >= d) {
+           cnt++;
+       }
+       
+       return depth[idx];
+   }
+}
