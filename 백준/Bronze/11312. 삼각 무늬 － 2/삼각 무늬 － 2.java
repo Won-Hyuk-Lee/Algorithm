5e541578082f75894c1c7f972c6e32a7620459da
이원혁 <104248767+Won-Hyuk-Lee@users.noreply.github.com>
2025-06-11 20:43:26 +0900

    [Bronze III] Title: 삼각 무늬 - 2, Time: 276 ms, Memory: 20612 KB -BaekjoonHub

---
+++ 백준/Bronze/11312. 삼각 무늬 － 2/삼각 무늬 － 2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            System.out.println((a / b) * (a / b));
+        }
+        sc.close();
+    }
+}
