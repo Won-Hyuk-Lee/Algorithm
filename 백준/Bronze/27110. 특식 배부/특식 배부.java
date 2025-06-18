e143d8793540001a016ae6a1f34713f1d17ad325
이원혁 <104248767+Won-Hyuk-Lee@users.noreply.github.com>
2025-06-18 20:31:04 +0900

    [Bronze IV] Title: 특식 배부, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += Math.min(n, Integer.parseInt(st.nextToken()));
+        }
+        System.out.println(sum);
+    }
+}
